3f4fb73d3dad869f419c13c642af6a6247b2914a
srgrover <jthmoya.webdev@gmail.com>
2017-05-11 14:36:44 +0200

    Enlace editar se muestra correctamente al pasar el raton en cards del perfil

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,12 +1,3 @@
 $(document).ready(function () {
-    $('.avatar img').mouseenter(function() {
-        $('.avatar a').css({
-            "display": "inline"
-        });
-    });
-    $('.avatar img').mouseleave(function() {
-        $('.avatar a').css({
-            "display": "none"
-        });
-    })
+
 });
